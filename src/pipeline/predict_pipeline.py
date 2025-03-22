349f678aa7f4c6f12ac7d6414d895763cd80853f
kamalesh <kamaleshbaskaran4310@gmail.com>
2025-03-22 11:57:10 +0530

    app.py update

--- src/pipeline/predict_pipeline.py
+++ src/pipeline/predict_pipeline.py
@@ -12,7 +12,7 @@ class PredictPipeline:
     def predict(self, features):
         try:
             model_path = os.path.join("artifacts", "model.pkl")
-            preprocessor_path = os.path.join("articats", "preprocessor.pkl")
+            preprocessor_path = os.path.join("artifacts", "preprocessor.pkl")
             print("Before Loading Model")
             model = load_object(file_path=model_path)
             preprocessor = load_object(file_path=preprocessor_path)
